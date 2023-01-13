6e6d48b4831503db7c04a1c296bcc0c054991fc4
Omeed Zarrabian <60447674+OmeedZ@users.noreply.github.com>
2023-01-13 02:33:26 -0500

    Update code.py

--- code.py
+++ code.py
@@ -3,12 +3,14 @@
 # Of the whole dataframe:
   df.describe()
    
+# One-hot encode columns
+  df = pd.get_dummies(df, columns= ['col1', 'col2', '...'])
+ 
 ## Copyable Code for Linear Regression:
-  
   # Simple Linear Regression
   from sklearn.model_selection import train_test_split
   from sklearn.linear_model import LinearRegression
-  X = df.drop('column1', 'column2', '...', axis = 1)
+  X = df.drop('column1', 'column2', '...', axis = 1) #usually just drop the target column, but can drop other unwanted columns as well
   Y = df['target_column']
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
   linear_model = LinearRegression()
@@ -22,8 +24,13 @@
     # Ridge Linear Regression (Used to tackle multicollinearity and if a dataset has a large amount of predictors)
     from sklearn.linear_model() import Ridge   
     ridge_model = Ridge(alpha=0.5, normalize = True) #increasing alpha may improve model by forcing important predictors to have more influence
-    ridge_model.fit(X_train, y_train)   
+    ridge_model.fit(X_train, y_train)
      
+    # SVM Linear Regression (Used for a small dataset, heavily penalizes outliers)
+    from sklearn.svm import SVR
+    regression_model = SVR(kernel = 'linear', C = 1.0) #C is the penalty parameter, can change this parameter as needed.
+    regression_model.fit(X_train, y_train)
+    
   # R2 score of the training data
   linear_model.score(X-train,Y-train)
   # R2 score of the test data
